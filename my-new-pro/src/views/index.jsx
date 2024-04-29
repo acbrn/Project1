7a4dfc1bacb23b7d59f019018807577089cc0bc2
Andrew_Coburn <coburn188@gmail.com>
2024-04-29 13:45:32 -0500

    Remove unused files and dependencies

--- my-new-pro/src/views/index.jsx
+++ my-new-pro/src/views/index.jsx
@@ -1,7 +1,7 @@
 export { default as Newsletter } from './newsletter/Newsletter.jsx';
 export { default as Latest } from './newsletter/Latest.jsx';
 export { default as Archive } from './newsletter/Archive.jsx';
-export { default as Services } from './servicesf/Services.jsx';
+export { default as Services } from './services/Services.jsx';
 export { default as Contact } from './contact/Contact.jsx';
 export { default as About } from './about/About.jsx';
 export { default as Home } from './home/Home.jsx';
