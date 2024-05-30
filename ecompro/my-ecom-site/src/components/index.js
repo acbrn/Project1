549f246016a71da074c9e32378beb1f8ecfe0f23
Andrew Coburn <coburn188@gmail.com>
2024-05-30 10:44:04 -0500

    Delete ecompro/my-ecom-site directory

--- ecompro/my-ecom-site/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Navbar } from './navbar/Navbar';
-export { default as Header } from './header/Header';
-export { default as Footer } from './footer/Footer';
-export { default as ProductItem } from './productitem/Productitem';
-export { default as ProductList } from './productlist/Productlist';
-export { default as CartItem } from './cartitem/CartItem'; // Renamed the file to 'CartItem.jsx'
